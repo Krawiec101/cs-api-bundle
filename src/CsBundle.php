d2f31872d72db2421454d6ad742e6949ef31dce0
Łukasz Krawczyk <lukaszkrawczyklodz@gmail.com>
2020-09-21 14:58:17 +0200

    initial commit

---
+++ src/CsBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CsApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Lukas Kahwe Smith <smith@pooteeweet.org>
+ * @author Eriksen Costa <eriksencosta@gmail.com>
+ */
+class CsBundle extends Bundle
+{
+
+}
